4d5bf7d5debf89c9474dd63842789a75e0ae9e52
TarynL <tarlytle@gmail.com>
2021-07-07 10:39:53 -0500

    getAll/GetAllComments methods updated

--- Streamish/Repositories/IVideoRepository.cs
+++ Streamish/Repositories/IVideoRepository.cs
@@ -6,6 +6,7 @@ namespace Streamish.Repositories
     public interface IVideoRepository
     {
         List<Video> GetAll();
+        List<Video> GetAllWithComments();
         Video GetById(int id);
         void Add(Video video);
         void Delete(int id);
