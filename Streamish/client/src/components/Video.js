1c82503c3f40e624e1a94e2a75e13622b02ab2f4
TarynL <tarlytle@gmail.com>
2021-07-09 15:49:04 -0500

    video details page functional

--- Streamish/client/src/components/Video.js
+++ Streamish/client/src/components/Video.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { Card, CardBody } from "reactstrap";
 
 const Video = ({ video }) => {
@@ -14,11 +15,13 @@ const Video = ({ video }) => {
                     allowFullScreen />
 
                 <p>
-                    <strong>{video.title}</strong>
+                    <Link to={`/videos/${video.id}`}>
+                        <strong>{video.title}</strong>
+                    </Link>
                 </p>
                 <p>{video.description}</p>
                 <div>
-                    Comments: {video.comments?.map((message) => (<ul>{message.message}</ul>))}
+                    {video.comments?.map((message) => (<ul>{message.message}</ul>))}
                 </div>
             </CardBody>
         </Card>
--- Streamish/client/src/modules/videoManager.js
+++ Streamish/client/src/modules/videoManager.js
@@ -19,3 +19,7 @@ export const addVideo = (video) => {
         body: JSON.stringify(video),
     });
 };
+
+export const getVideo = (id) => {
+    return fetch(`${baseUrl}/${id}`).then((res) => res.json());
+};
